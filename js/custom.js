04360c1a7183d3a5289013e59eefbc406c1b6e46
BuildTools <ismeazamsakib@gmail.com>
2021-11-24 11:16:56 +0600

    completed till team

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    //banner slider;
+    $('.banner-slider').slick({
+        arrows: false,
+        dots:true,
+    });
+
+    //about video popup
+    $('.venobox').venobox(); 
+});
