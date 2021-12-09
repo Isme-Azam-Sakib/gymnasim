18cc8f39ac836c3e08ac6653dc46e6d08562776a
BuildTools <ismeazamsakib@gmail.com>
2021-12-09 14:38:04 +0600

    design complete

--- js/custom.js
+++ js/custom.js
@@ -15,4 +15,19 @@ $(document).ready(function(){
         slidesToShow:2,
         dots: true,
     });
+
+
+    //counter
+    $('.counter').counterUp();
+
+
+    //partner slider
+    $('.partner-slider').slick({    
+        slidesToShow: 5,
+        centerMode:true,
+        centerPadding: 0,
+        prevArrow: '<i class="fas fa-chevron-left slick-prev prev-arrow"></i>' ,
+        nextArrow: '<i class="fas fa-chevron-right slick-next next-arrow"></i>' ,
+    });
+
 });
